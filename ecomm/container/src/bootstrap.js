6a2813ac6be22674358c557356f5b7bb09ddb394
Kristian T. <57638182+kristian4res@users.noreply.github.com>
2025-12-29 20:13:43 +0000

    ref: use mount pattern for agnostic comp rendering

--- ecomm/container/src/bootstrap.js
+++ ecomm/container/src/bootstrap.js
@@ -1,3 +1,5 @@
-import 'products/ProductsIndex';
+import { mount as productsMount } from 'products/ProductsIndex';
 import 'cart/CartShow';
+
 console.log("MFE container");
+productsMount(document.querySelector("#dev-products"));
--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,9 +1,21 @@
 import faker from "faker";
 
-let products = "";
-for (let i = 0; i < 3; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`
+const mount = (element) => {
+    let products = "";
+    for (let i = 0; i < 3; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`
+    }
+
+    element.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === "development") {
+    const el = document.querySelector("#dev-products");
+
+    if (el) {
+        mount(el);
+    }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+export { mount };
