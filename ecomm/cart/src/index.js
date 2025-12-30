15ef18c747523caa6bc8a9702c547f25f137eacb
Kristian T. <57638182+kristian4res@users.noreply.github.com>
2025-12-30 18:35:41 +0000

    chore: rename first app root dir

--- ecomm/cart/src/index.js
+++
@@ -1,2 +0,0 @@
-// Allows webpack to load files asynchronously and fetch dependencies before execution (REQUIRED when marking shared dependencies)
-import('./bootstrap');
--- ecomm/container/src/index.js
+++
@@ -1,2 +0,0 @@
-// Allows webpack time to load and read the requirements of the file before execution
-import("./bootstrap.js");
