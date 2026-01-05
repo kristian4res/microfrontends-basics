90097687b4017d8b944305e57dc4c920a419a92c
Kristian T. <57638182+kristian4res@users.noreply.github.com>
2026-01-05 18:29:33 +0000

    feat: use react

---
+++ ecomm-2/packages/container/src/components/MarketingApp.js
@@ -0,0 +1,12 @@
+import { mount as marketingMount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+
+export default () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        mount(ref.current);
+    });
+
+    return <div ref={ref} />
+};
