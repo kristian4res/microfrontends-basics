5171c4d71ecfa5dea2379b1e82615cde34472d1a
kristian4res <torreskristian77@gmail.com>
2026-01-10 17:15:55 +0000

    fix(marketing): correct webpack config typos and add dev div element; chore: install deps

--- ecomm-2/packages/marketing/config/webpack.common.js
+++ ecomm-2/packages/marketing/config/webpack.common.js
@@ -2,7 +2,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.m?s$/,
+                test: /\.m?js?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
