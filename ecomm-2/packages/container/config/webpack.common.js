b90c23197750dc520448c44a3e4b658395a27ec8
kristian4res <torreskristian77@gmail.com>
2026-01-10 17:16:52 +0000

    fix(container): correct webpack config typos; chore: install deps

--- ecomm-2/packages/container/config/webpack.common.js
+++ ecomm-2/packages/container/config/webpack.common.js
@@ -1,8 +1,11 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
 module.exports = {
     module: {
         rules: [
             {
-                test: /\.m?s$/,
+                test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
@@ -13,5 +16,10 @@ module.exports = {
                 }
             }
         ]
-    }
-}
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+};
