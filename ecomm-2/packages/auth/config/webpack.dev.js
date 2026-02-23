94a609731fe0f19d8c6338acd6dda6381fb21eff
kristian4res <torreskristian77@gmail.com>
2026-02-23 19:46:37 +0000

    feat: config files for dashboard subapp

--- ecomm-2/packages/auth/config/webpack.dev.js
+++ ecomm-2/packages/auth/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const devConfig = {
     mode: "development",
@@ -22,6 +23,9 @@ const devConfig = {
                 "./AuthApp": "./src/bootstrap",
             },
             shared: packageJson.dependencies,
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
         }),
     ],
 };
