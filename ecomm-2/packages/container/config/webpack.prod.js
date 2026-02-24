f2b7b6b0f282dab015039531abb4f7774c77f209
kristian4res <torreskristian77@gmail.com>
2026-02-24 21:18:07 +0000

    build: update config to include dashboard

--- ecomm-2/packages/container/config/webpack.prod.js
+++ ecomm-2/packages/container/config/webpack.prod.js
@@ -1,24 +1,26 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
-    mode: 'production',
+    mode: "production",
     output: {
-        filename: '[name].[contenthash].js'
+        filename: "[name].[contenthash].js",
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'container',
+            name: "container",
             remotes: {
                 marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+                auth: `auth@${domain}/auth/remoteEntry.js`,
+                dashboard: `dashboard@${domain}/dashboard/remoteEntry.js`,
             },
-            shared: packageJson.dependencies
+            shared: packageJson.dependencies,
         }),
-    ]
+    ],
 };
 
 module.exports = merge(commonConfig, prodConfig);
